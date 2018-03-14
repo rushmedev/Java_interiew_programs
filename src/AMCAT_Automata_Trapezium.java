bbbda62225248ab515b8148570c643078a3264e3
Rashmi Ranjan Bisoi <rushmedev@gmail.com>
2018-03-14 15:46:54 +0530

    Print Trapezium pattern in Java | Cognizant PAT | AMCAT Automata | Mindtree Coding round.

---
+++ src/AMCAT_Automata_Trapezium.java
@@ -0,0 +1,21 @@
+public class AMCAT_Automata_Trapezium {
+    public static void main(String[] args) {
+        int a = 0, b = 0, n = 4;
+        for (int i = n; i >= 1; i--) {
+            for (int j = n; j >= i; j--) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= i; k++) {
+                System.out.print(++a + "*");
+            }
+            for (int l = 1; l <= i; l++) {
+                System.out.print(++b + i * i);
+                if (l != i) {
+                    System.out.print("*");
+                } else {
+                    System.out.println("");
+                }
+            }
+        }
+    }
+}
